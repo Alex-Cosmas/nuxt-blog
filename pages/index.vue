753aee84a26b0a94547778e0cd2a026a3b496a2f
Alex Cosmas <cosmasalex365@gmail.com>
2023-07-28 14:43:23 +0300

    Set up Nuxt blog

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const { $notion } = useNuxtApp();
+const { data } = await useAsyncData("notion-index", () =>
+  $notion.getPageBlocks("677dcadf924c4cdcae3198c047c18245")
+);
+const { mapPageUrl, pageLinkOptions } = useProps();
+</script>
+
+<template>
+  <div>
+    <NotionRenderer
+      :blockMap="data"
+      fullPage
+      prism
+      katex
+      :mapPageUrl="mapPageUrl"
+      :pageLinkOptions="pageLinkOptions"
+    />
+  </div>
+</template>
